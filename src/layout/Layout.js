d9c5628dc21558e9b24fd0e035d9174d2d284839
jan soriano <jansoriano.work@gmail.com>
2022-11-13 10:35:16 +0800

    tab title and favicon update

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,15 +1,21 @@
 import React from 'react'
-
+import Head from 'next/head'
 import Footer from '../components/Footer/Footer'
 import Header from '../components/Header/Header'
 import { Container } from './LayoutStyles'
 
 export const Layout = ({children}) => {
   return (
+    <div>
+    <Head>
+    <title>Jan Vincent Soriano</title>
+    <meta property="og:Jan Vincent Soriano" content="Jan Vincent Soriano Dev Portfolio" key="title" />
+  </Head>
     <Container>
      <Header/>
      <main>{children}</main> 
      <Footer/>
     </Container>
+    </div>
   )
 }
