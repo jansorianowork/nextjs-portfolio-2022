a10f10baea8f7065d66808fc0a8544e1de43c696
jansorianowork <jansoriano.work@gmail.com>
2023-05-09 22:36:45 +0800

    updated cv

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,7 +7,7 @@ import { projects } from '../../constants/constants';
 const Projects = () => (
   <Section id="projects">
     <SectionDivider colorAlt/>
-    <SectionTitle main>Projects</SectionTitle>
+    <SectionTitle main>Companies</SectionTitle>
     <GridContainer>
       {projects.map((p, i) => {
         return (
