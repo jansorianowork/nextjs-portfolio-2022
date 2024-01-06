96a08938930ee2c2e2b02a57e6372a6a27cafe36
jansorianowork <jansoriano.work@gmail.com>
2024-01-06 15:55:56 +0800

    update portfolio 2024

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -10,7 +10,10 @@ const Hero = (props) => (
           Web Developer Portfolio
         </SectionTitle>
         <SectionText>
-        A Front-End Developer with experience in JavaScript, ReactJs, Typescript, Dynamics 365 Portal, Shopify theme development, HTML, CSS, SCSS, Storybook, and Liquid templating. Has eight years of experience working in the IT industry. He began his career as a software support in an accounting firm, focusing mainly on the system UI. He also has interests in software principles such as DRY and YAGNI, DevOps deployment methodology, Agile methodology, and web3.
+        A Front-End Developer with experience in JavaScript, ReactJs, Typescript, Dynamics 365 Portal, Shopify theme development, HTML, CSS, SCSS, Storybook, and Liquid templating. 
+        <br/>Has eight years of experience working in the IT industry. He began his career as a software support and pursued a career in web development. 
+        
+        <br/>He also has interests in software principles such as DRY and YAGNI, DevOps deployment methodology, Agile methodology, and web3.
         </SectionText>
       </LeftSection>
     </Section>
