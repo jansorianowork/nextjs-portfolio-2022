e4a943fcfe0912aa6210be12939c5b3cc98af3df
jansorianowork <jansoriano.work@gmail.com>
2025-12-09 23:48:50 +0800

    upgrade to version 16

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	// Enable styled-components support in the Next compiler
+	compiler: {
+		styledComponents: true,
+	},
+};
+
+module.exports = nextConfig;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,49 +1,58 @@
-import Link from 'next/link';
-import React from 'react';
-import { AiFillGithub, AiOutlineCloudDownload, AiFillLinkedin } from 'react-icons/ai';
-import { DiTerminal } from 'react-icons/di';
+import Link from "next/link";
+import React from "react";
+import {
+	AiFillGithub,
+	AiOutlineCloudDownload,
+	AiFillLinkedin,
+} from "react-icons/ai";
+import { DiTerminal } from "react-icons/di";
 
+import {
+	Container,
+	Div1,
+	Div2,
+	Div3,
+	NavLink,
+	SocialIcons,
+} from "./HeaderStyles";
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
-
-const Header = () =>  (
-  <Container>
-    <Div1>
-      <Link href="/">
-        <a style={{ display: 'flex', alignItems: 'center', color:"white" }}>
-          <DiTerminal size="3rem" /> <span>Jan Soriano</span>
-        </a>
-      </Link>
-    </Div1>
-    <Div2>
-      <li>
-        <Link href="#projects">
-          <NavLink>Experience</NavLink>
-        </Link>
-      </li>
-      <li>
-        <Link href="#tech">
-          <NavLink>Technologies</NavLink>
-        </Link>
-      </li>        
-      <li>
-        <Link href="#about">
-          <NavLink>Progression</NavLink>
-        </Link>
-      </li>        
-    </Div2>
-      <Div3>
-        <SocialIcons href="https://github.com/jansorianowork">
-          <AiFillGithub size="3rem" />
-        </SocialIcons>
-        <SocialIcons href="https://www.linkedin.com/in/jansoriano-233984192/">
-          <AiFillLinkedin size="3rem" />
-        </SocialIcons>
-        <SocialIcons href="file/JVSorianoCV2024.pdf">
-          <AiOutlineCloudDownload size="3rem"/>
-        </SocialIcons>
-      </Div3>
-    </Container>
+const Header = () => (
+	<Container>
+		<Div1>
+			<Link
+				href="/"
+				style={{
+					display: "flex",
+					alignItems: "center",
+					color: "white",
+				}}
+			>
+				<DiTerminal size="3rem" /> <span>Jan Soriano</span>
+			</Link>
+		</Div1>
+		<Div2>
+			<li>
+				<Link href="#projects">Experience</Link>
+			</li>
+			<li>
+				<Link href="#tech">Technologies</Link>
+			</li>
+			<li>
+				<Link href="#about">Progression</Link>
+			</li>
+		</Div2>
+		<Div3>
+			<SocialIcons href="https://github.com/jansorianowork">
+				<AiFillGithub size="3rem" />
+			</SocialIcons>
+			<SocialIcons href="https://www.linkedin.com/in/jansoriano-233984192/">
+				<AiFillLinkedin size="3rem" />
+			</SocialIcons>
+			<SocialIcons href="file/JVSorianoCV2024.pdf">
+				<AiOutlineCloudDownload size="3rem" />
+			</SocialIcons>
+		</Div3>
+	</Container>
 );
 
 export default Header;
