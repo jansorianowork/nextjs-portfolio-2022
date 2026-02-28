e86313a3fd96c0ad08a43538fb3d243b30ed4f7e
jansorianowork <jansoriano.work@gmail.com>
2026-02-28 19:32:38 +0800

    update changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,26 @@
-import Acomplishments from '../components/Acomplishments/Acomplishments';
-import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation';
-import Hero from '../components/Hero/Hero';
-import Projects from '../components/Projects/Projects';
-import Technologies from '../components/Technologies/Technologies';
+import Acomplishments from "../components/Acomplishments/Acomplishments";
+import BgAnimation from "../components/BackgrooundAnimation/BackgroundAnimation";
+import Hero from "../components/Hero/Hero";
+import Projects from "../components/Projects/Projects";
+import Technologies from "../components/Technologies/Technologies";
 // import Pricing from '../components/Pricing/Pricing';
-import Timeline from '../components/TimeLine/TimeLine';
-import { Layout } from '../layout/Layout';
-import { Section } from '../styles/GlobalComponents';
+import Timeline from "../components/TimeLine/TimeLine";
+import { Layout } from "../layout/Layout";
+import { Section } from "../styles/GlobalComponents";
 
 const Home = () => {
-  return (
-    <Layout>
-      <Section grid>
-        <Hero />
-        <BgAnimation />
-      </Section>
-      <Projects />
-      <Technologies />
-      <Timeline />
-      <Acomplishments />
-      {/* <Pricing /> */}
-    </Layout>
-  );
+	return (
+		<Layout>
+			<Section grid>
+				<Hero />
+			</Section>
+			<Projects />
+			<Technologies />
+			<Timeline />
+			<Acomplishments />
+			{/* <Pricing /> */}
+		</Layout>
+	);
 };
 
 export default Home;
